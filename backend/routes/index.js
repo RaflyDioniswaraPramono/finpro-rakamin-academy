8f0b48922079a3795e45804a21de9a484ef0f223
Rafly Dioniswara Pramono <raflypram368@gmail.com>
2023-11-21 18:48:31 +0700

    Adding new suppliers controller and admin login API

---
+++ backend/routes/index.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const supplierReportRoutes = require("./supplierReportRoutes");
+
+router.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Api is working!",
+  });
+});
+
+router.get("/api/v1", (req, res) => {
+  res.status(200).json({
+    message: "Api is working!",
+  });
+});
+
+router.use("/api/v1", supplierReportRoutes);
+
+module.exports = router;

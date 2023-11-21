8007e5d96ed09231c0bfca1787cac66eddd25f49
Rafly Dioniswara Pramono <raflypram368@gmail.com>
2023-11-21 11:50:48 +0700

    Starter Project Backend

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.DEV_PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`App server is running on port ${PORT}!`);
+})
